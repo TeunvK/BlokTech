eef85eaa1c9b8415ef58e75b1f803d6f2330e3c9
teun <teun.van.kessel@hva.nl>
2022-03-08 12:06:26 +0100

    added templating engine

--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express')
+const {engine} = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+
+app.use(express.static(__dirname + '/static'));
+
 app.get('/', (req, res) => {
-  res.send('Hello Matching App!')
+  res.render('home');
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    person: {
+      firstname: "Hans",
+      lastname: "Anders",
+    } 
+  });
+})
+
+app.get('*', (req, res) => {
+  res.send('404')
+  
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on localhost:${port}`)
 })
