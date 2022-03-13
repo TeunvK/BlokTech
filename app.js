b52b9e442e5142570b6b5e1f8558137f27f263b4
teun <teun.van.kessel@hva.nl>
2022-03-13 17:41:44 +0100

    added eslinter

--- app.js
+++ app.js
@@ -1,57 +1,58 @@
-const express = require('express');
-const {engine} = require('express-handlebars');
-const PORT = process.env.PORT || 3000 
-require('dotenv').config()
-const connectDB = require('./config/db')
+const express = require("express");
+const {
+	engine
+} = require("express-handlebars");
+const PORT = process.env.PORT || 3000;
+require("dotenv").config();
+const connectDB = require("./config/db");
 
-const app = express()
+const app = express();
 
 connectDB();
 
-app.engine('handlebars', engine({
-  defaultLayout: 'main',
-  layoutsDir: __dirname + '/views/layouts',
-  partialsDir: __dirname +'/views/partials'
+app.engine("handlebars", engine({
+	defaultLayout: "main",
+	layoutsDir: __dirname + "/views/layouts",
+	partialsDir: __dirname + "/views/partials"
 }));
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.set("view engine", "handlebars");
+app.set("views", "./views");
 
 
-app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + "/static"));
 
-app.get('/', (req, res) => {
-  res.render('login');
-})
+app.get("/", (req, res) => {
+	res.render("login");
+});
 
-app.get('/match', (req, res) => {
-  res.render('match', {
-    shows: [
-      {
-      name: 'Falling skies',
-      image: 'Falling skies image'
-    },
-     {
-      name: "Game of Thrones",
-      image: "Game of Thrones image"
-    }
-  ]
+app.get("/match", (req, res) => {
+	res.render("match", {
+		shows: [{
+			name: "Falling skies",
+			image: "Falling skies image"
+		},
+		{
+			name: "Game of Thrones",
+			image: "Game of Thrones image"
+		}
+		]
 
-  });
-})
+	});
+});
 
-app.get('/about', (req, res) => {
-  res.render('about', {
-    person: {
-      firstname: "Hans",
-      lastname: "Anders",
-    }
-  });
-})
+app.get("/about", (req, res) => {
+	res.render("about", {
+		person: {
+			firstname: "Hans",
+			lastname: "Anders",
+		}
+	});
+});
 
-app.get('*', (req, res) => {
-  res.send('Error 404')
-})
+app.get("*", (req, res) => {
+	res.send("Error 404");
+});
 
 app.listen(PORT, () => {
-  console.log(`App listening on localhost:${PORT}`)
-})
+	console.log(`App listening on localhost:${PORT}`);
+});
