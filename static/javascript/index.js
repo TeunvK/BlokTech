16101452d8565e5b05c4b5431b56bfde37e00575
TeunvK <ijsthee98@gmail.com>
2022-04-09 16:17:37 +0200

    working on fetch show search results

--- static/javascript/index.js
+++ static/javascript/index.js
@@ -1,10 +1,19 @@
 console.log("test");
 
-
-async function getTvShows(){
-
-	const res = await fetch("https://www.episodate.com/api/}");
-	console.log(res);
+async function getTvShows(query){
+	const res = await fetch(`https://www.episodate.com/api/search?q=${query}`);
+	const data = await res.json();
+	const result = await data.map(test => test.name);
+	console.log(result)
 }
 
-getTvShows();
+
+
+
+window.onload = (data) => {
+	const searchTerm = document.getElementById("tvShow");
+	searchTerm.onkeyup = (event) => {
+		getTvShows(searchTerm.value);
+	}
+	console.log(data)
+}
